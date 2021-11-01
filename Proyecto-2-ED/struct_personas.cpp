5750852bc79b21c91790b27b68b2d6aa6bc12cda
Chacalerks <erksmartinez2014@gmail.com>
2021-10-31 23:47:37 -0600

    Actualización versión 1.0

---
+++ Proyecto-2-ED/struct_personas.cpp
@@ -0,0 +1,144 @@
+#include "struct_personas.h"
+
+bool ListaDoblePersonas::isEmpty(){
+    return primerNodo == NULL;
+}
+
+bool ListaDoblePersonas::insertar(Persona * persona){
+    NodoPersona *nuevo = new NodoPersona(persona);
+    if (isEmpty()){
+        primerNodo = ultimoNodo = nuevo;
+
+    }else{
+        NodoPersona *temp = primerNodo;
+        while (temp->persona->id < persona->id){
+            qDebug()<<temp->persona->id;
+            temp = temp->siguiente;
+            if(temp == NULL) break;
+        }
+        if(temp == NULL){
+            ultimoNodo->siguiente = nuevo;
+            nuevo->anterior = ultimoNodo;
+            ultimoNodo = nuevo;
+
+        }else if(temp->persona->id == persona->id){
+            return false;
+        }else{
+            if(temp == primerNodo){
+                primerNodo->anterior = nuevo;
+                nuevo->siguiente = primerNodo;
+                primerNodo = nuevo;
+            }else{
+                nuevo->siguiente = temp;
+                nuevo->anterior = temp->anterior;
+                temp->anterior->siguiente = nuevo;
+                temp->anterior = nuevo;
+            }
+
+        }
+    }
+    largo++;
+    return true;
+
+}
+
+void ListaDoblePersonas::imprimir(){
+    NodoPersona * temp = primerNodo;
+    while (temp != NULL){
+        temp->persona->imprimir();
+        temp = temp->siguiente;
+    }
+    qDebug()<<"\nLargo: "<<largo;
+}
+
+
+
+
+
+
+
+//===============================================TREE==============================================================================
+
+void ArbolPersonas::insertar(int dato, NodoPersona* nodoListaPersona){
+     raiz = insertar(dato, raiz, nodoListaPersona);
+     cantElementos++;
+}
+
+//  INSERTA RECURSIVAMENTE
+NodoPersonaArbol* ArbolPersonas::insertar(int valor, NodoPersonaArbol* nodo, NodoPersona* nodoListaPersona){
+    if (nodo == NULL){
+        return new NodoPersonaArbol(nodoListaPersona);
+
+    }else if (nodo->nodoPersona->persona->id < valor){
+        nodo->hijoderecho = insertar(valor, nodo->hijoderecho, nodoListaPersona);
+
+    }else if (nodo->nodoPersona->persona->id >= valor){
+        nodo->hijoizquierdo = insertar(valor, nodo->hijoizquierdo, nodoListaPersona);
+    }
+    return nodo;
+}
+//  VACIAR EL: ARBOL
+void ArbolPersonas::vaciarArbol(){
+    vaciarArbol(raiz);
+     raiz = NULL;
+     cantElementos = 0;
+}
+
+
+void ArbolPersonas::vaciarArbol(NodoPersonaArbol* nodo){
+    if (nodo != NULL){
+        vaciarArbol(nodo->hijoizquierdo);
+        vaciarArbol(nodo->hijoderecho);
+        nodo = NULL;
+        delete(nodo);
+    }
+}
+
+
+//BUSCAR EL MAS CERCANO
+NodoPersona* ArbolPersonas::buscarMasCercano(int datoID){
+    if (raiz != NULL)return NULL;
+
+    /*if(raiz->nodoPersona->persona->id == dato)return raiz->nodoPersona;
+
+    else if(raiz->nodoPersona->persona->id < dato){
+        return buscarMasCercano(dato,'d', abs(raiz->nodoPersona->persona->id - dato), raiz->hijoderecho)->nodoPersona;
+    }else{
+        return buscarMasCercano(dato,'i', abs(raiz->nodoPersona->persona->id - dato), raiz->hijoizquierdo)->nodoPersona;
+    }*/
+
+    buscarMasCercano(raiz, datoID);
+
+
+}
+
+NodoPersonaArbol* ArbolPersonas::diferencia(NodoPersonaArbol * nodo1, NodoPersonaArbol * nodo2, int dato){
+    int diferencia1 = abs(nodo1->nodoPersona->persona->id - dato);
+    int diferencia2 = abs(nodo2->nodoPersona->persona->id - dato);
+
+    if(diferencia1 < diferencia2)        return nodo1;
+    else       return nodo2;
+}
+
+NodoPersonaArbol* ArbolPersonas::buscarMasCercano(NodoPersonaArbol * raizNodo,int datoID){
+
+    if(raizNodo->hijoizquierdo != NULL  &&  raizNodo->hijoderecho != NULL){
+       return diferencia(raizNodo,buscarMasCercano(diferencia(raizNodo->hijoizquierdo, raizNodo->hijoderecho,datoID),datoID), datoID);
+
+    }else{
+        return raizNodo;
+    }
+
+}
+
+
+void ArbolPersonas::posOrden(NodoPersonaArbol* nodo)
+{
+   if (nodo != NULL)
+   {
+     posOrden(nodo->hijoizquierdo);
+     posOrden(nodo->hijoderecho);
+     qDebug() << nodo->nodoPersona->persona->id << "  ";
+
+   }
+}
---
+++ Proyecto-2-ED/struct_personas.h
@@ -0,0 +1,189 @@
+#ifndef STRUCT_PERSONAS_H
+#define STRUCT_PERSONAS_H
+
+#include <QString>
+#include <QDateTime>
+#include <QCoreApplication>
+#include <QFile>
+#include <QTextStream>
+
+//-------------------------------------------------------
+struct Pecado{
+
+    QString name;
+    int cant;
+
+    Pecado(){
+        cant = 0;
+        name = "";
+    }
+    Pecado(QString _name, int _cant){
+        cant = _cant;
+        name = _name;
+    }
+
+};
+
+
+//-------------------------------------------------------------
+struct BuenaAccion{
+
+    QString name;
+    int cant;
+
+    BuenaAccion(){
+        cant = 0;
+        name = "";
+    }
+    BuenaAccion(QString _name, int _cant){
+        cant = _cant;
+        name = _name;
+    }
+
+};
+
+struct Persona{
+    int id;
+    QString name;
+    QString apellido;
+    QString pais;
+    QString creencia;
+    QString profesion;
+    QString email;
+    bool vivo;
+    QDateTime nacimiento;
+    Pecado * pecados[7];
+    BuenaAccion * buenasAcciones[7];
+
+    Persona(){
+        id = 0;
+        name = "";
+        apellido = "";
+        pais = "";
+        creencia = "";
+        profesion = "";
+        email = "ytcesarjs@gmail.com";
+        vivo = true;
+        nacimiento = QDateTime::currentDateTime();
+        for(int i = 0; i<7; i++){
+            pecados[i] = new Pecado();
+            buenasAcciones[i] = new BuenaAccion();
+        }
+    }
+
+    Persona(int _id, QString _name, QString _apellido, QString _pais, QString _creencia, QString _profesion){
+        id = _id;
+        name = _name;
+        apellido = _apellido;
+        pais = _pais;
+        creencia = _creencia;
+        profesion = _profesion;
+        email = "erksmartinez2014@gmail.com";
+        vivo = true;
+        nacimiento = QDateTime::currentDateTime();
+        for(int i = 0; i<7; i++){
+            pecados[i] = new Pecado();
+            buenasAcciones[i] = new BuenaAccion();
+        }
+    }
+
+    void imprimir(){
+        qDebug()<<"\n-----------------\nID: "<<id<<"\nNombre: "<<name<<"\nApellido: "<<apellido<<"\nContador: "<<creencia;
+    }
+
+};
+
+struct NodoPersona{
+    Persona * persona;
+    NodoPersona * siguiente;
+    NodoPersona * anterior;
+
+    NodoPersona(){
+        persona = new Persona;
+        siguiente = NULL;
+    }
+    NodoPersona(Persona * _persona){
+        persona = _persona;
+        siguiente = anterior = NULL;
+    }
+};
+
+struct ListaDoblePersonas{
+    NodoPersona * primerNodo;
+    NodoPersona * ultimoNodo;
+    int largo;
+    ListaDoblePersonas(){
+        primerNodo = ultimoNodo = NULL;
+        largo = 0;
+    }
+
+    bool isEmpty();
+    bool insertar(Persona * persona);
+    void imprimir();
+    void insertarALInicio(NodoPersona * persona);
+    void insertarAlFinal(NodoPersona * persona);
+
+    void imprimirReverse();
+    NodoPersona * borrarALInicio();
+    NodoPersona * borrarALFinal();
+    NodoPersona * buscar(NodoPersona * persona);
+    void insertarEn(int index, NodoPersona * persona);
+};
+
+
+//-------------Arbol-----------------------
+struct NodoPersonaArbol{
+    NodoPersona * nodoPersona;
+    NodoPersonaArbol * hijoizquierdo,*hijoderecho;
+
+    NodoPersonaArbol(){
+        nodoPersona = new NodoPersona();
+        hijoizquierdo = hijoderecho = NULL;
+    }
+
+    NodoPersonaArbol(NodoPersona * _persona){
+        nodoPersona = _persona;
+        hijoizquierdo = hijoderecho = NULL;
+    }
+};
+
+struct ArbolPersonas{
+    NodoPersonaArbol * raiz;
+    int cantElementos;
+    ArbolPersonas(){
+        raiz = NULL;
+        cantElementos =0;
+    }
+
+    void insertar (int, NodoPersona*);
+    NodoPersonaArbol * insertar (int, NodoPersonaArbol*, NodoPersona*);
+
+    void vaciarArbol();
+    void vaciarArbol(NodoPersonaArbol*);
+
+    NodoPersona* buscarMasCercano(int);
+    NodoPersonaArbol * diferencia(NodoPersonaArbol*,NodoPersonaArbol*,int);
+    NodoPersonaArbol * buscarMasCercano(NodoPersonaArbol *,int );
+
+    void posOrden(NodoPersonaArbol*);
+    void imprimirUltimo(NodoPersonaArbol*);
+
+    void inOrden(NodoPersonaArbol*);
+    void inOrdenClientes(NodoPersonaArbol*);
+    void preOrden(NodoPersonaArbol*);
+
+    NodoPersonaArbol* buscar (int, NodoPersonaArbol*);
+    int obtenerNumeroElementos(NodoPersonaArbol*);
+    int peso (NodoPersonaArbol*);
+    int obtenerAltura(NodoPersonaArbol*);
+    NodoPersonaArbol* mayor (NodoPersonaArbol* arbol);
+    NodoPersonaArbol* borrarElemento(int);
+    NodoPersonaArbol* borrarElemento(int, NodoPersonaArbol*);
+    void anchura(NodoPersonaArbol*);
+    int altura(NodoPersonaArbol*);
+    int cantHojas(NodoPersonaArbol*);
+    int cantNodos2(NodoPersonaArbol*);
+    void nodos_nivel (NodoPersonaArbol*, int, int);
+    int contadorNodos(NodoPersonaArbol*);
+};
+#endif // STRUCT_PERSONAS_H
